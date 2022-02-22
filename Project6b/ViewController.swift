3227b18ba0d5e2de62d7d73719bf3d1201f06341
Oscarlui <oscarlu1@connect.hku.hk>
2022-02-23 01:01:02 +0800

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Oscar Lui on 23/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
